e35a1c2e9a48d16ebe8a7c515fce5107a495b244
Daniel Íñiguez <60960955+diniguezcabrera@users.noreply.github.com>
2021-04-12 17:33:23 +0200

    fix timezone parse

--- parse_feed.py
+++ parse_feed.py
@@ -1,6 +1,6 @@
 import os
+from time import mktime
 from datetime import datetime, timedelta
-import dateutil.parser
 import discord
 import feedparser
 from urls import URLS
@@ -19,12 +19,12 @@ async def on_ready():
         entries = feed.get('entries')
 
         for entry in entries:
-            published = dateutil.parser.parse(entry.published)
+            published_datetime = datetime.fromtimestamp(mktime(entry.published_parsed))
 
-            if datetime.now(published.tzinfo) - timedelta(hours=6) < published:
+            if datetime.now() - timedelta(hours=6) < published_datetime:
                 title = entry.title
                 link = entry.links[0].get('href')
-                verbose_published = published.strftime('%d/%m/%Y, %H:%M')
+                verbose_published = published_datetime.strftime('%d/%m/%Y, %H:%M')
                 channel = client.get_channel(DISCORD_CHANNEL_ID)
                 await channel.send(f'[{verbose_published}]\n{title}\n{link}')
 
